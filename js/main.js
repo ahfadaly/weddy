a7284b9058728ce1c259d547cc7ba4baac7d60e2
ahfadaly <fadaly.dev@gmail.com>
2024-07-11 13:28:44 +0300

    Weddy

---
+++ js/main.js
@@ -0,0 +1,74 @@
+"use strict";
+
+// add class active -----------
+let navlinks = document.querySelectorAll(".header .list li a");
+
+navlinks.forEach((item) => {
+  item.addEventListener(`click`, () => {
+    document.querySelector(".header .list li a.active").classList.remove("active");
+    item.classList.add("active");
+  });
+});
+
+// AOS ANIMATION ON SCROLL
+AOS.init({
+  duration: 1000,
+  easing: "ease",
+  once: true,
+});
+
+// carousel ---------
+
+$(`document`).ready(() => {
+  // partners
+  $(".our-partners .owl-carousel").owlCarousel({
+    loop: true,
+    margin: 15,
+    rtl: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    dots: false,
+    nav: true,
+    navText: [
+      '<button class="slide-arrow prev-arrow"><i class="fa-solid fa-chevron-left"></i></button>',
+      '<button class="slide-arrow prev-arrow"><i class="fa-solid fa-chevron-right"></i></button>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      769: {
+        items: 2,
+      },
+      1200: {
+        items: 3,
+      },
+    },
+  });
+
+  //  customer
+  $(".customer-opinions .owl-carousel").owlCarousel({
+    loop: true,
+    margin: 15,
+    rtl: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    dots: false,
+    nav: true,
+    navText: [
+      '<button class="slide-arrow prev-arrow"><i class="fa-solid fa-chevron-left"></i></button>',
+      '<button class="slide-arrow prev-arrow"><i class="fa-solid fa-chevron-right"></i></button>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      769: {
+        items: 2,
+      },
+      1200: {
+        items: 3,
+      },
+    },
+  });
+});
